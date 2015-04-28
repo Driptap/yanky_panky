b407d840d9d756c818faeb41ab38f0a407fcb720
driptap <sg.sinan@gmail.com>
2015-04-28 16:06:02 +0100

    added models

--- db/migrate/20150428145644_create_tracks.rb
+++ db/migrate/20150428145644_create_tracks.rb
@@ -2,8 +2,8 @@ class CreateTracks < ActiveRecord::Migration
   def change
     create_table :tracks do |t|
       t.string :file_name
-      t.int :play_count
-      t.int :skip_count
+      t.integer :play_count
+      t.integer :skip_count
       t.string :name
       t.string :artist
       t.string :album
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150428145644) do
+
+  create_table "tracks", force: true do |t|
+    t.string   "file_name"
+    t.integer  "play_count"
+    t.integer  "skip_count"
+    t.string   "name"
+    t.string   "artist"
+    t.string   "album"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "auth_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
