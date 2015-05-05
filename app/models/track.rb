ca954dc36b3244d3574c313ebc8af25aa5adcf9a
driptap <sg.sinan@gmail.com>
2015-05-05 18:10:48 +0100

    modified env vars

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,7 @@
 class Track < ActiveRecord::Base
 	belongs_to :user
 	scope :unplayed,	-> { where(play_count: 0) }
+
 	# Updates the track play count
 	def is_being_played!
 		 Track.update_counters(self, play_count: 1)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :tracks
+	#validates_presence_of :db_user_id, :auth_token
+	#validates_uniqueness_of :db_user_id
  	# Returns a predicted new track
 	def find_track
 		if self.tracks.length >= 1	
